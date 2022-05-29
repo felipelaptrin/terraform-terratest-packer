aeb52069a45f3fc053782e4d777270e016767caa
felipe trindade <felipelaptrin@gmail.com>
2022-05-29 18:24:09 -0300

    added infra test

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -19,9 +23,21 @@ func TestTerraform(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
-	ec2_dummy_tags := terraform.OutputMap(t, terraformOptions, "ec2_dummy_tags")
-	s3_bucket_tags := terraform.OutputMap(t, terraformOptions, "s3_bucket_tags")
-
-	assert.Equal(t, EXPECTED_DEFAULT_TAGS, ec2_dummy_tags)
-	assert.Equal(t, EXPECTED_DEFAULT_TAGS, s3_bucket_tags)
+	// Validate EC2 and S3
+	resources_tags := [3]string{
+		"ec2_dummy_tags",
+		"s3_bucket_tags",
+		"ec2_cluster_tags",
+	}
+	for i := 0; i < len(resources_tags); i++ {
+		tag := terraform.OutputMap(t, terraformOptions, resources_tags[i])
+		assert.Equal(t, EXPECTED_DEFAULT_TAGS, tag)
+	}
+	time.Sleep(30 * time.Second)
+	// Validate static application
+	DNS_ALB := "http://" + terraform.Output(t, terraformOptions, "alb_dns")
+	http_helper.HttpGetWithCustomValidation(t, DNS_ALB, nil, func(status int, content string) bool {
+		return status == 200 &&
+			strings.Contains(content, "<img src=\"./static/logo.png\" alt=\"Flugel logo\" class=\"center\">")
+	})
 }
